e8a5f94b2ffe922166d06ada1835951763da7795
Astrid Bossy <astrid.bossy@Digital-Grenoble.local>
2018-05-24 09:38:49 +0200

    refactorisation detailprojet

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -2,7 +2,7 @@
                 
  
 @section('titre')
-<h1>Liste des Projets</h1>
+<h1><strong>Liste des Projets</strong></h1>
 
 @endsection
 
@@ -14,8 +14,8 @@
         <div class="col-lg-offset-2 col-lg-7"></div>
         <h2>{{$projet->title}}</h2>
         <a href="{{route('detailprojet',[$projet->id])}}"><img src="image/australie.png" alt="photo australie"></a> 
-        <p>{{$projet->content}}</p>
-        <a href='{{route('detailprojet',[$projet->id])}}'<button id="Contribuer au Projet"
+       
+        <a href="{{route('detailprojet',[$projet->id])}}"<button id="Contribuer au Projet"
          name="Contribuer au Projet" class="btn btn-info">VOIR LE PROJET</button></a>
     </div>
     @endforeach
