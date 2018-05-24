c4650e0d46256775b0d624ee7e0f921464cb87c8
Astrid Bossy <astrid.bossy@Digital-Grenoble.local>
2018-05-24 15:43:56 +0200

    changement d'image sur projectFactory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Project::class, function ($faker) {
     $user= factory(App\User::class)->create();
     return [
         'title' => $faker->sentence(),
-        'image'=>('https://images.unsplash.com/photo-1490535004195-099bc723fa1f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=53d16e51c95625168dde435dbd0aaa45&auto=format&fit=crop&w=1480&q=80'),
+        'image'=>('https://images.unsplash.com/photo-1508163223045-1880bc36e222?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ff0cb0a7ffd2e11c7bfb064b1229babc&auto=format&fit=crop&w=1351&q=80'),
         'content' => implode($faker->paragraphs(5)),
         'created_at' => \Carbon\Carbon::now(),
         'updated_at' => \Carbon\Carbon::now(),
