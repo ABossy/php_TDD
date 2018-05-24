a2fe5e69163cd4c9cec2a2ff600d79dba66a6735
Astrid Bossy <astrid.bossy@Digital-Grenoble.local>
2018-05-24 15:39:26 +0200

    Modif formulaire edition

--- resources/views/editproject.blade.php
+++ resources/views/editproject.blade.php
@@ -6,26 +6,29 @@
 @endsection
 
 @section('form')
-<div class="container">
-	<form class="well span8">
+<form action='{{route('projet.modif',[$project->id])}}' method="POST">
+    {{csrf_field()}}
+    <div class="container">
         <div class="row">
             <div class="span3">
-                <label>Name</label> 
-                <input class="span3" placeholder="Your Name" type="text"><br>
-                <label>Email</label> <input class="span3" placeholder=
-                "Your email address" type="text"><br>
-                <label>Project Title</label>
-                <input class="span3" placeholder="Your Project" type="text">
-            </div>
-    
-            <div class="span5">
-                <label>Message</label><br> 
+                <label>Titre</label> 
+                <input class="span3" name="title" type="text">
+                <label>image</label> 
+                <input class="span3" type="url"  name="website"><hr>
+                <label>date de Modification</label>
+                <input class="span3" name="date" type="date"><hr>
+                <label>Detail du projet</label><br> 
                 <textarea class="input-xlarge span5" id="message" name="message"
-                rows="10" cols="70">
-    </textarea>
-            </div><button class="btn btn-primary pull-center" type=
-            "submit">Send</button>
-        </div>
-    </form>
-</div>
+                rows="10" cols="80"></textarea>
+            </div>    
+        </div>     
+    </div>
+<!-- Button Modifier -->	
+    <div class="form-group">
+	  <label class="col-md-4 control-label" for=""></label>
+	  <div class="col-md-4">
+	    <button id="" name="" class="btn btn-warning" type="submit">Modifier</button>
+	  </div>
+	</div>
+</form>
 @endsection
