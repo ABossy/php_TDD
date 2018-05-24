35916e7a56a9f51427341659e39e1d2aac8be99e
Astrid Bossy <astrid.bossy@Digital-Grenoble.local>
2018-05-24 15:37:09 +0200

    ajout des routes création + modification

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// page liste des projets
 Route::get('/project','ProjectController@index');
 
+// page detail du projet
 Route::get('/project/show/{id}','ProjectController@show')->name('detailprojet');
 
 Route::get('/pagedon', function (){
 return view('pagedon');
 });
 
-Route::get('/editproject')->name('editproject')->middleware('auth');
-    
+Route::get('/creation', function (){
+    return view('create');
+    });
+
+// page création d'un projet
+Route::post('/edit','ProjectController@store')->name('create')->middleware('auth');
+
+// page edition d'un projet
+Route::get('/editproject/{id}','ProjectController@edit')->name('editproject')->middleware('auth');
+
+// page modification d'un projet
+Route::post('/modif/{id}','ProjectController@modifproject')->name('projet.modif')->middleware('auth');
     
 
 
